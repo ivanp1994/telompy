b3b597c5d942d7f3444ca11eb8c7de5d36bb202d
Ivan Pokrovac <ivan.pokrovac.fbf@gmail.com>
2024-05-09 16:21:37 +0200

    Merged init from master

--- telompy/__init__.py
+++ telompy/__init__.py
@@ -4,5 +4,8 @@ Created on Wed May  8 10:18:14 2024
 
 @author: ivanp
 """
+
 from .funcs import fish_last_label, calculate_telomere_lengths
-from .cli import command_line_target as pali
+from .cli import command_line_target as cli_start
+
+__all__ = ["fish_last_label", "calculate_telomere_lengths", "cli_start"]
