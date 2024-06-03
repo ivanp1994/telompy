1df987b0b003c9fffbfd33c04db9597d39cfce4c
Ivan Pokrovac <ivan.pokrovac.fbf@gmail.com>
2024-06-03 08:29:58 +0200

    Pulled readme and added it to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 
 from setuptools import setup, find_packages
 
-# with open("README.md", "r") as _f:
-#    long_description = _f.read()
+with open("README.md", "r") as _f:
+    long_description = _f.read()
 
 with open("requirements_dev.txt", "r") as _f:
     extra_reqs = _f.read().strip().split("\n")
@@ -14,9 +14,9 @@ setup(name="TelOMpy",
       version="0.0.1",
       description="Python based calculator for length of telomers from BNGO de novo assembly",
       packages=find_packages(),
-      #      long_description=long_description,
-      #      long_description_content_type='text/markdown',
-      #      url="",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url="",
       author="Ivan Pokrovac",
       author_email="ivan.pokrovac.fbf@gmail.com",
       license="MIT",
